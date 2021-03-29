742bcdb9c1511c04aba24ef6e44de7b5078affc0
Julien Fleche <julien.fleche@ebikelabs.com>
2021-03-29 14:04:19 +0200

    Added write result on file

--- pedaling_mgt/main.c
+++ pedaling_mgt/main.c
@@ -8,6 +8,12 @@
 
 int main(void)
 {
+    FILE *fp;
+
+    fp = fopen("pedaling_speed.csv", "w+");
+
+    fprintf(fp, "pedaling_speed,average_pedaling_speed\n");
+
     for (uint8_t i = 0; i < NUMBER_OF_INTERRUPTION; i++)
     {
         update_timestamp();
@@ -19,10 +25,12 @@ int main(void)
 
         /* USER CODE END */
 
-        printf("Interrupt number: %d\n", i);
-        printf("Pedaling speed: %2.1f\n", get_pedaling_speed());
-        printf("Average pedaling speed: %2.1f\n\n", get_average_pedaling_speed());
+        // Save data on CSV file
+        fprintf(fp, "%2.1f,%2.1f\n", get_pedaling_speed(), get_average_pedaling_speed());
     }
+
+    // Close CSV file
+    fclose(fp);
 
     return 0;
 }
